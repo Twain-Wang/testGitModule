1f52201f23376b14369e1be33d4cc47f418fefb5
Dawn.Wang <dawnwang@DawnWangdeMacBook-Pro.local>
2016-06-06 10:03:21 +0800

    first commit

---
+++ testGitModule/testGitModule.h
@@ -0,0 +1,13 @@
+//
+//  testGitModule.h
+//  testGitModule
+//
+//  Created by Dawn.Wang on 16/6/6.
+//  Copyright (c) 2016年 xml. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testGitModule : NSObject
+-(NSString*)setName;
+@end
---
+++ testGitModule/testGitModule.m
@@ -0,0 +1,16 @@
+//
+//  testGitModule.m
+//  testGitModule
+//
+//  Created by Dawn.Wang on 16/6/6.
+//  Copyright (c) 2016年 xml. All rights reserved.
+//
+
+#import "testGitModule.h"
+
+@implementation testGitModule
+-(NSString *)setName
+{
+    return @"小飞象";
+}
+@end
